a9164958016db871ebd565eaf13f5eae4853357d
NikitaMalaviya <143192567+NikitaMalaviya@users.noreply.github.com>
2023-08-25 18:42:04 +0530

    Add files via upload

---
+++ DEMO.C
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+/*
+  Keyword =
+  const
+
+
+ */
+main()
+{
+    int a = 10;
+    clrscr();
+
+      a = 15;
+      a= 20;
+      printf("A = %d",a);
+
+    getch();
+}
---
+++ TEST.C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+	clrscr();
+	int marks = 50;
+	printf("Marks = %d",marks);
+
+	getch();
+}
